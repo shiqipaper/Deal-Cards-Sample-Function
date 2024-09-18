54f76a0e5055c3edb7ecf8de8747405ce82a2c40
luqi <lq.xie.work@gmail.com>
2024-09-17 17:23:38 -0700

    finish with for and while loop

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ HAND_SIZE = 10
 def build_deck():
     return [f"{card}{suit}" for card in CARDS for suit in SUITS]
 
+
+
+def sample(items, count):
+    # card_list = []
+    # while len(card_list) < count: 
+    #     card_index = random.randint(0,len(items) - 1)
+    #     if not items[card_index] in card_list:
+    #         card_list.append(items[card_index])
+
+    card_list = []        
+    deck_copy = list(items)
+    for _ in range(10):
+        card_index = random.randint(0, len(deck_copy)-1)
+        card_list.append(deck_copy[card_index])
+        deck_copy.pop(card_index)
+
+    return card_list
+
+
+
 # write a function that deals 10 cards from a deck of cards without using the sample function
 def deal_ten_cards(deck_of_cards):
-    pass
+    return sample(deck_of_cards, HAND_SIZE)
+
+deck = build_deck()
+print(deck)
+print(deal_ten_cards(deck))
